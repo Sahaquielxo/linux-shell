8c3bc4abb4307a4fe9d867895d1cea558615a216
Viktor <v.khilin@carprice.ru>
2017-08-17 12:48:54 +0300

    docker_images_clean.sh can calculate how many free space you have after images deleting

--- Docker-Images-Cleaner/docker_images_clean.sh
+++ Docker-Images-Cleaner/docker_images_clean.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 #Will remove unused images, when there are no containers based in this image in the system
+img_size=0
 for i in `docker images -q` 
 do 
   imgcont=`docker ps -af "ancestor=$i" | sed '1d' | awk '{print $NF}'`
   if [ `docker ps -af "ancestor=$i" | sed '1d' | wc -l` -eq 0 ] 
   then 
 	echo "Image "$i" is unsued. We will remove it now"
-	 docker rmi "$i"
+	img_size_new=`docker images | grep "$i" | awk '{print $NF}' | sed -r 's/[A-Za-z]//g'`
+ 	img_size=$(($img_size + $img_size_new))
+	docker rmi "$i"
    else
 	 echo "Image "$i" is used in "$imgcont" container, skip"
   fi 
 done
+if [ "$img_size" -gt 0 ]
+then
+	echo "After unused images delete, you have ${img_size}MB more free space on your disk"
+fi
